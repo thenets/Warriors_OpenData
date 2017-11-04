af7b89bcf7a275a100615f74c0992e82da75f8e9
Luiz Felipe F M Costa <phelip@thenets.org>
2017-11-04 21:07:14 +0000

    Before force encode using parse to JSON

--- examples/xml2dict.py
+++ examples/xml2dict.py
@@ -1,4 +1,4 @@
-import requests, xmltodict, unicodedata
+import requests, xmltodict, unicodedata, collections
 
 # XML url
 XML_URL = 'https://raw.githubusercontent.com/servicosgovbr/cartas-de-servico/master/cartas-servico/v3/lista-completa-servicos.xml'
@@ -6,15 +6,30 @@ XML_URL = 'https://raw.githubusercontent.com/servicosgovbr/cartas-de-servico/mas
 # Get XML from URL
 with requests.Session() as session:
     request = session.get(XML_URL)
+    content = request.content
     
-    print(request.encoding)
-    exit(1)
+    # Parse XML to dict
+    CONTENT_DICT = xmltodict.parse(content)
     
     # Encode (remove special characters)
-    encoded_content = unicodedata.normalize('NFKD', str(request.content))
-
-    # Parse XML to dict
-    CONTENT_DICT = xmltodict.parse(encoded_content)
+    def normalize_dict(d):
+        # Check if is iterable but not string
+        try:
+            if(not isinstance(d, str)):
+                # Recursive method call
+                for i in d:
+                    print('class', d.__class__)
+                    normalize_dict(i)
+            
+        except Exception as e:
+            print(e)
+            if(isinstance(d, str)):
+                d = unicodedata.normalize('NFKD', str(d)).encode('ASCII','ignore').decode('ASCII')
+                print(d)
+            
+    normalize_dict(CONTENT_DICT)
+                
+        
 
 # Example of output
-print(CONTENT_DICT)
+# print(CONTENT_DICT)
